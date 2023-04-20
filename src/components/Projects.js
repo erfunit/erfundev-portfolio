fc6b9a87aade2019275404574a9b08e5a7eb0094
erfunit <erfunghodoosi@gmail.com>
2023-04-21 02:30:55 +0430

    change succeeded

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,22 @@
+import todo from "../assets/todo.jpg";
+import hoobank from "../assets/hoobank.jpg";
+import growwithdata from "../assets/growithdata.jpg";
+
 export const projects = [
   {
-    thumbnail: "../assets/todo.jpg",
+    thumbnail: todo,
     title: "todo application",
     techs: "React / Tailwindcss",
     githubLink: "https://github.com/erfunit/to-do",
   },
   {
-    thumbnail: "../assets/hoobank.jpg",
+    thumbnail: hoobank,
     title: "Hoobank UI implementation",
     techs: "React / Tailwindcss",
     githubLink: "https://github.com/erfunit/bank-modern-app",
   },
   {
-    thumbnail: "../assets/growithdata.jpg",
+    thumbnail: growwithdata,
     title: "Grow with data UI implementation",
     techs: "React / Tailwindcss",
     githubLink: "https://github.com/erfunit/grow-with-data",
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,7 @@
 import { useContext } from "react";
-import todoThumbnail from "../assets/todo.jpg";
-import hooBank from "../assets/hoobank.jpg";
-import growWithData from "../assets/growithdata.jpg";
+import { projects } from "./Projects";
 import { mouseContext } from "../contexts/mouseContext";
+import { FaEye, FaGithub } from "react-icons/fa";
 
 const Works = () => {
   const [hoverable, setHoverable] = useContext(mouseContext);
@@ -15,77 +14,54 @@ const Works = () => {
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-14">
           {/*  */}
-          <div
-            onMouseOver={() => {
-              setHoverable((pre) => true);
-            }}
-            onMouseLeave={() => {
-              setHoverable((pre) => false);
-            }}
-            className="group"
-          >
-            <div className="overflow-hidden mb-5 rounded">
-              <img
-                src={todoThumbnail}
-                alt=""
-                className="group-hover:scale-105 transition-all duration-700"
-              />
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              onMouseOver={() => {
+                setHoverable((pre) => true);
+              }}
+              onMouseLeave={() => {
+                setHoverable((pre) => false);
+              }}
+              className="group"
+            >
+              <div className="overflow-hidden rounded relative border border-white/10 mb-5 ">
+                <img
+                  src={project.thumbnail}
+                  alt=""
+                  className="group-hover:scale-105 transition-all duration-700"
+                />
+                <div className="absolute hidden opacity-0 gap-x-3 transition-all group-hover:opacity-100 h-full w-full bg-gray-800/60 z-20 top-0 left-0 md:flex justify-center items-center flex-row">
+                  <a
+                    target="_black"
+                    href="https://github.com/erfunit/grow-with-data"
+                    className="p-4  rounded bg-white/40 text-white transition-all hover:bg-white/60 active:scale-95"
+                  >
+                    <FaGithub
+                      size={25}
+                      className="rounded-full opacity-0  scale-50 group-hover:opacity-100 group-hover:scale-100 delay-150 transition-all "
+                    />
+                  </a>
+                  <a
+                    target="_black"
+                    href="https://github.com/erfunit/grow-with-data"
+                    className="p-4 rounded bg-white/40 text-white transition-all hover:bg-white/60 active:scale-95"
+                  >
+                    <FaEye
+                      size={25}
+                      className="rounded-full opacity-0  scale-50 group-hover:opacity-100 group-hover:scale-100 delay-100 transition-all "
+                    />
+                  </a>
+                </div>
+              </div>
+              <span className="font-nunito font-light text-base md:text-lg text-[#828282]">
+                {project.techs}
+              </span>
+              <h2 className="text-[#E9E9E9] font-nunito font-bold text-xl md:text-xl">
+                {project.title}
+              </h2>
             </div>
-            <span className="font-nunito font-light text-base md:text-lg text-[#828282]">
-              ReactJS / Tailwindcss
-            </span>
-            <h2 className="text-[#E9E9E9] font-nunito font-bold text-xl md:text-xl">
-              todo application
-            </h2>
-          </div>
-          {/*  */}
-          <div
-            onMouseOver={() => {
-              setHoverable((pre) => true);
-            }}
-            onMouseLeave={() => {
-              setHoverable((pre) => false);
-            }}
-            className="group"
-          >
-            <div className="overflow-hidden mb-5 rounded">
-              <img
-                src={hooBank}
-                alt=""
-                className="group-hover:scale-105 transition-all duration-700"
-              />
-            </div>
-            <span className="font-nunito font-light text-base md:text-lg text-[#828282]">
-              ReactJS / Tailwindcss
-            </span>
-            <h2 className="text-[#E9E9E9] font-nunito font-bold text-xl md:text-xl">
-              Hoobank UI implementation
-            </h2>
-          </div>
-          {/*  */}
-          <div
-            onMouseOver={() => {
-              setHoverable((pre) => true);
-            }}
-            onMouseLeave={() => {
-              setHoverable((pre) => false);
-            }}
-            className="group"
-          >
-            <div className="overflow-hidden mb-5 rounded">
-              <img
-                src={growWithData}
-                alt=""
-                className="group-hover:scale-105 transition-all duration-700"
-              />
-            </div>
-            <span className="font-nunito font-light text-base md:text-lg text-[#828282]">
-              ReactJS / Tailwindcss
-            </span>
-            <h2 className="text-[#E9E9E9] font-nunito font-bold text-xl md:text-xl">
-              Grow with data UI implementation
-            </h2>
-          </div>
+          ))}
         </div>
       </div>
     </div>
